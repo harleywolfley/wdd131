49efd77a1336f63720a663b41b869e449a1d8ca4
harleywolfley <harleywolfley@gmail.com>
2025-09-27 18:12:59 -0600

    mission update

---
+++ mission/mission.js
@@ -0,0 +1,19 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.classList.add('dark');
+        logo.src = 'byui-logo_white.png';
+    } else {
+        document.body.classList.remove('dark');
+        logo.src = 'byuilogo.png';
+    }
+}    
+
+themeSelector.addEventListener('change', changeTheme);
+                    
