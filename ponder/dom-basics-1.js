f5afe98e891366ce4363c667c773b61dc54c64bf
harleywolfley <harleywolfley@gmail.com>
2025-09-30 13:42:41 -0600

    please man

---
+++ ponder/dom-basics-1.js
@@ -0,0 +1,11 @@
+const newP = document.createElement('p');
+newP.textContent = "This is a new paragraph added by JavaScript.";
+document.body.append(newP);
+
+const newImg = document.createElement('img');
+newImg.src = "https://picsum.photos/300";
+newImg.setAttribute('alt', 'Reandom image from picsum.');
+document.body.appendChild(newImg);
+
+newDiv.innerHTML = `<ul><li>$mylist`
+
