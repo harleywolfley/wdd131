f4cdccd4c5d77b41fb55196334600941a9495449
harleywolfley <harleywolfley@gmail.com>
2025-10-22 16:35:42 -0600

    cool pics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,8 +1,31 @@
-element.addEventListener("event", handlerFunction)
-const menuButton = document.querySelector(".menu-button");
-function toggleMenu() {
-    const menu = document.querySelector("menu-button");
-    menu.classList.toggle("hide");
+function handleResize() {
+  const menu = document.querySelector(".menu");
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
 }
 
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+
+const menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", toggleMenu);
+window.addEventListener("resize", handleResize);
+handleResize();
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('.modal');
+gallery.addEventListener('click', (event) => {
+  if (event.target.tagName === 'IMG') {
+    modal.showModal();
+  }
+})
+
+const closeButton = document.querySelector('.close-viewer')
+closeButton.addEventListener('click', () => {
+    modal.close()
+});
