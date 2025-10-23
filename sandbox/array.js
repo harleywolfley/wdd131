35b63bcbd42a1753dc34af000aec5820bd852929
harleywolfley <harleywolfley@gmail.com>
2025-10-23 11:49:09 -0600

    arrays

--- sandbox/array.js
+++ sandbox/array.js
@@ -5,3 +5,30 @@ function listTemplate(step) {
 }
 const stepsHtml = steps.map(listTemplate);
 document.querySelector("#myList").innerHTML = stepsHtml.join("");
+
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade) {
+  let points = 0;
+  if (grade === "A") {
+    points = 4;
+  } else if (grade === "B") {
+    points = 3;
+  }
+  return points;
+}
+const gpaPoints = grades.map(convertGradeToPoints);
+
+const pointsTotal = gpaPoints.reduce(function(total, item) {
+  return total + item;
+})
+
+const gpa = pointsTotal / gpaPoints.length;
+
+const words = ["watermelon", "peach", "apple", "tomato", "grape"]
+const shortWords = words.filter(function(word) {
+  return word.length < 6;
+});
+
+const index = [12, 34, 21, 54]
+const luckyNumber = 21;
+let luckIndex = index.indexOf(luckyNumber);
